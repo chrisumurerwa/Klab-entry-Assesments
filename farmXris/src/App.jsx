53a45ce05600c223fec75e1ed258e150a2a7ddbe
chrisumurerwa <chrisumurerwa1@gmail.com>
2025-03-18 22:59:26 +0200

    added content in mentorship program

--- farmXris/src/App.jsx
+++ farmXris/src/App.jsx
@@ -7,6 +7,8 @@ import Home from "./Components/Home";
 import Feed from "./Components/Feed";
 import Farming from "./Components/Farming";
 import Contact from "./Components/Contact";
+import Mentorship from "./Components/Mentorship";
+import Veterinary from "./Components/Veterinary";
 import Main from "./Dashboard/components/Main";
 import DashboardLayout from "./Dashboard/components/DashboardLayout";
 
@@ -26,7 +28,9 @@ function App() {
               <Route path="/Home" element={<Home />} />
               <Route path="/Feed" element={<Feed />} />
               <Route path="/Farming" element={<Farming/>}/>
+              <Route path="/Mentorship" element={<Mentorship/>}/>
               <Route path="/Contact" element={<Contact />} />
+              <Route path="/Veterinary" element={<Veterinary/>}/>
             </Route>
 
             <Route path="/Login" element={<Login/>} />
---
+++ farmXris/src/Components/Veterinary.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../Styles/Veterinary.css'
+function Veterinary() {
+  return (
+    <div>Veterinary</div>
+  )
+}
+export default  Veterinary;
